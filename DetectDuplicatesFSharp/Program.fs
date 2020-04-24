f58d4b3b4e21a11e42ca4f6ad57aa2ec7d74a906
Paul Blasucci <pblasucci@gmail.com>
2020-04-24 21:20:23 +0200

    an imperfect attempt

--- DetectDuplicatesFSharp/Program.fs
+++ DetectDuplicatesFSharp/Program.fs
@@ -1,81 +1,53 @@
-﻿open System.IO
-open System.Linq
+﻿module DetectDuplicates.Program
+
+open System.IO
 open System.Text.RegularExpressions
 
-type FileReader = string -> Result<string seq,exn>
+let [<Literal>] Okay = 0
+let [<Literal>] Fail = 1
 
-let output (data:seq<string*string>) =
-    data 
-    |> Seq.iter (fun (key,value) -> printfn "Duplicate %s found in %s" key value)
+let findDuplicates file packages =
+  packages
+  |> Seq.groupBy id
+  |> Seq.choose (fun (package, occurs) ->
+      if 1 < Seq.length occurs then
+        Some {| FileName = file; Package = package |}
+      else None
+  )
 
-let getPath (args: string []) =
-    match args.Any() with
-    | true -> args.[0]
-    | false -> Directory.GetCurrentDirectory()
-
-let getFiles path =
-    printfn "Looking for duplicates in %s" path
-    Directory.GetFiles(path, "*.csproj", SearchOption.AllDirectories)
-
-let readFile : FileReader =
-    fun path ->
-        try
-            seq { use reader = new StreamReader(File.OpenRead(path))
-                  while not reader.EndOfStream do
-                      yield reader.ReadLine() }
-            |> Ok
-        with
-        | ex -> Error ex
-        
-let parseLine (fileName:string) (line:string)  =
-    let regexMatch = Regex.Match(line, "PackageReference Include=\"([^\"]*)\"")
-    match regexMatch.Success with
-    | true -> 
-        Some (regexMatch.Groups.[1].Value, fileName)
-    | _ -> None
-
-let parse (fileName:string) (data:string seq) =
-    data
-    |> Seq.map (fun x-> parseLine fileName x)
-    |> Seq.choose id
-    
-    
-let findDuplicates (packages:seq<string * string>) =
-    packages
-    |> Seq.groupBy (fun s -> s)
-    |> Seq.filter (fun (key, values) -> (values.Count() >= 2)) 
-    |> Seq.map (fun(key,_)-> key)
-    
-let checkFile (file: string) =
-    let fileName = Path.GetFileName(file)
-    try
-        match file |> readFile with
-        | Ok data -> data |> parse fileName |> findDuplicates   
-        | Error ex ->   printfn "Error: %A" ex.Message
-                        Seq.empty<string*string>
-    with
-    | ex -> printfn "Error processing entry in %s" fileName
-            Seq.empty<string*string>
-
-let loopFiles (files: string []) =
-    Array.map (fun x -> checkFile x) files
-
-let processProjectFiles (args: string []) =
-    args
-    |> getPath
-    |> getFiles
-    |> loopFiles
-    |> Seq.collect (fun x->x)
+let getPackages file =
+  let packages = seq {
+    for line in File.ReadLines(file) do
+      let matched = Regex.Match(line, "PackageReference Include=\"([^\"]*)\"")
+      if matched.Success then matched.Groups.[1].Value
+  }
+  {| File = file; Packages = packages |}
 
 [<EntryPoint>]
-let main argv =
+let main args =
+  try
+    let folder =
+      args
+      |> Array.tryHead
+      |> Option.defaultWith Directory.GetCurrentDirectory
 
-    let dups = processProjectFiles argv
-    dups |> output
+    printfn "Looking for duplicates in %s... " folder
 
-    printfn "Duplicate detection complete"
-    
-    if dups.Any() then
-        1
+    let files =
+      Directory.EnumerateFiles(folder, "*.csproj", SearchOption.AllDirectories)
+
+    let duplicates = seq {
+      for entry in files |> Seq.map getPackages do
+        yield! findDuplicates entry.File entry.Packages
+    }
+
+    printfn "Duplicate detection complete!"
+
+    if 1 < Seq.length duplicates then
+      for dup in duplicates do
+        eprintfn "Duplicate %s found in %s" dup.Package dup.FileName
+      Fail
     else
-        0
+      Okay
+  with
+  | x -> eprintfn "ERROR! %A" x; Fail
